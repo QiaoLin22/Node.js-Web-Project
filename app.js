0df39f9a9c375a79cf5e518f3935ff2477e38167
ElaineDong98 <32375876+ElaineDong98@users.noreply.github.com>
2019-10-14 23:15:06 -0400

    added the layer
    added main ejs and home ejs, as well as app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("homepage");
+});
+
+app.get("/main", function(req, res){
+    var posts = [
+        {title: "Wagaya Japanese Restaurant!", image:"../login/static/image/Cherry.png", tags:"restaurants"},
+        {title: "Tanaka", image:"../login/static/image/Cherry.png", tags:"restaurants"},
+    ]
+    res.render("main");
+});
+
+app.get("/register", function(req, res){
+
+});
+
+app.listen(3000);
+/*
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The DooleyEats Server has Started");
+});
+*/
